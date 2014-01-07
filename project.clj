608796108702720f1f6562d1c3b770ef397be419
Gary Deer <gdeer81@gmail.com>
2014-01-06 21:04:53 -0600

    stumbling around with testing.

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "A pharmacy system that uses Datomic"
   :url "https://github.com/gdeer81/functional-pharmacy"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.datomic/datomic-free "0.9.4384"]]
+                 [com.datomic/datomic-free "0.9.4384"]
+                 [expectations "1.4.56"]]
   :main functional-pharmacy.core
   :profiles {:uberjar {:aot :all}})
--- test/functional_pharmacy/core_test.clj
+++ test/functional_pharmacy/core_test.clj
@@ -1,6 +1,19 @@
 (ns functional-pharmacy.core-test
-  (:require [clojure.test :refer :all]
-            [functional-pharmacy.core :refer :all]))
+  (:require [expectations :refer :all]
+            [functional-pharmacy.core]
+            [datomic.api :as d]))
 
+(def db-url "datomic:mem://testy-db")
+(d/create-database db-url)
+(def conn (d/connect db-url))
 
-
+(def schema (read-string (slurp "resources/db/schema.edn")))
+(def people (read-string (slurp "resources/db/people-data.edn")))
+people
+(def db (:db-after @(d/transact conn  schema)))
+
+;;DO NOT DO THIS (def people-db @(d conn people))
+
+(d/q '[:find ?name
+       :in $
+       :where [_ :person/name ?name]] (:db-after (d/with db people)))
