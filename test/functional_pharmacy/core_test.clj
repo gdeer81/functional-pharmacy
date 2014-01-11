1f3a0050ba69326aa472c2702e1ba0b8fa800712
Gary Deer <gdeer81@gmail.com>
2014-01-11 16:40:25 -0600

    added test cases

--- test/functional_pharmacy/core_test.clj
+++ test/functional_pharmacy/core_test.clj
@@ -9,11 +9,17 @@
 
 (def schema (read-string (slurp "resources/db/schema.edn")))
 (def people (read-string (slurp "resources/db/people-data.edn")))
-people
+
 (def db (:db-after @(d/transact conn  schema)))
 
 ;;DO NOT DO THIS (def people-db @(d conn people))
 
-(d/q '[:find ?name
+(ffirst (d/q '[:find ?name
        :in $
-       :where [_ :person/name ?name]] (:db-after (d/with db people)))
+       :where [_ :person/name ?name]] (:db-after (d/with db people))))
+
+(d/q '[:find ?e ?birthdate
+       :in $
+       :where [?e :person/name "Clairol Danish"]
+              [?e :person/born ?birthdate]] (:db-after (d/with db people)))
+
