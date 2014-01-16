002f213dcd8a5103136397acc1011aa4afbb1cce
pance <pbarnett888@gmail.com>
2014-01-15 21:11:26 -0600

    Add test namespaces for the user action namespaces

---
+++ test/functional_pharmacy/patient_actions_test.clj
@@ -0,0 +1,20 @@
+(ns functional_pharmacy.patient_actions)
+
+;; we either need to write a function for each attribute that could be updated or we need to have that as a parameter
+;; just take a map?
+;; maybe a map of just the attributes to update, so you can
+;; update a hwole bunch at once
+(defn update-info
+  "Update the patient's information."
+  [] ;take a map?
+  nil)
+
+(defn view-prescription
+  "View the prescription for the patient."
+  []
+  nil)
+
+(defn create-fill ;; ???
+  "Create a fill transaction for a prescription"
+  []
+  nil)
